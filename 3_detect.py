da8321b8abe566d40844e004dc5b84d52630c9c4
jeongjoonsup <penny4860@gmail.com>
2018-01-20 20:15:57 +0900

    detected directory name added

--- 3_detect.py
+++ 3_detect.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 import os
 from src.net import text_activation_map
 
-WEIGHT_FILE = "weights//weights.16.h5"
+WEIGHT_FILE = "weights//weights.07-0.05.h5"
 
 
 if __name__ == "__main__":
@@ -18,7 +18,13 @@ if __name__ == "__main__":
         fig, ax = plt.subplots()
         ax.imshow(im, alpha=0.5)
         ax.imshow(text_map, cmap='jet', alpha=0.5)
+        
+        weight_name = os.path.split(WEIGHT_FILE)[1]
+        dname = os.path.join("images", "detected_{}".format(weight_name))
+        
+        if not os.path.isdir(dname):
+            os.mkdir(dname)
     
-        write_path = os.path.join("images", "detected", os.path.split(fname)[1])
+        write_path = os.path.join(dname, os.path.split(fname)[1])
         plt.savefig(write_path, bbox_inches='tight')
         print(write_path)
