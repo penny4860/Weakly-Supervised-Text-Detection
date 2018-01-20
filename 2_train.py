49eb2b05b40bc99706065ebca5451cde93764bc6
jeongjoonsup <penny4860@gmail.com>
2018-01-20 21:19:45 +0900

    train dataset path changed

--- 2_train.py
+++ 2_train.py
@@ -3,8 +3,8 @@ from src.net import mobilenet_binary_classifier
 from src.utils import build_generator, create_callbacks
 
 mobilenet = mobilenet_binary_classifier()
-train_generator, valid_generator = build_generator(train_directory="dataset//train",
-                                                   valid_directory="dataset//val")
+train_generator, valid_generator = build_generator(train_directory="..//weak_dataset//train",
+                                                   valid_directory="..//weak_dataset//val")
 mobilenet.fit_generator(train_generator,
                         steps_per_epoch = len(train_generator),
                         validation_data = valid_generator,
