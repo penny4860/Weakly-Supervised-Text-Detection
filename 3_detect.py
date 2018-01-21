e10af79b70c9459fa1e740dae4ebf4f0fa24249e
jeongjoonsup <penny4860@gmail.com>
2018-01-21 15:58:42 +0900

    nit : detect input images changed

--- 3_detect.py
+++ 3_detect.py
@@ -10,8 +10,8 @@ WEIGHT_FILE = "weights//weights.07-0.05.h5"
 
 if __name__ == "__main__":
 
-    img_files = os.listdir("images")
-    img_files = [os.path.join("images", fname) for fname in img_files]
+    img_files = os.listdir("dataset//train//text")
+    img_files = [os.path.join("dataset//train//text", fname) for fname in img_files]
     weight_name = os.path.split(WEIGHT_FILE)[1]
     dname = os.path.join("images", "detected_{}".format(weight_name))
     if not os.path.isdir(dname):
