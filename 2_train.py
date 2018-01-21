4485e44edf95d030515608c16c4a2a77418486e7
jeongjoonsup <penny4860@gmail.com>
2018-01-21 12:35:12 +0900

    training sample 로만 학습

--- 2_train.py
+++ 2_train.py
@@ -3,12 +3,12 @@ from src.net import mobilenet_binary_classifier
 from src.utils import build_generator, create_callbacks
 
 mobilenet = mobilenet_binary_classifier()
-train_generator, valid_generator = build_generator(train_directory="..//weak_dataset//train",
-                                                   valid_directory="..//weak_dataset//val")
+train_generator, valid_generator = build_generator(train_directory="dataset//train",
+                                                   valid_directory=None)
 mobilenet.fit_generator(train_generator,
                         steps_per_epoch = len(train_generator),
-                        validation_data = valid_generator,
-                        validation_steps = len(valid_generator),
+#                         validation_data = valid_generator,
+#                         validation_steps = len(valid_generator),
                         callbacks        = create_callbacks(),
                         epochs=20)
 
