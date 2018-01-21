6dde822f40bfa12bd59ce801e1a0735b98a20701
jeongjoonsup <penny4860@gmail.com>
2018-01-21 12:30:51 +0900

    Revert "sample 숫자를 지정하는 option added"
    This reverts commit 580ee04f11fe05a541d98988e564a562cf64d021.

--- 1_build_dataset.py
+++ 1_build_dataset.py
@@ -25,26 +25,21 @@ def to_file(img, directory, fname):
     path = os.path.join(directory, fname)
     print(path)
     cv2.imwrite(path, img)
+    
 
-def _get_n_samples(n_samples):
-    n_train_samples = n_samples
-    n_test_samples = int(n_samples / 5)
-    return n_train_samples, n_test_samples
-
-def write_cifar10_file(directory=DATASET_FOLDER, n_samples=50000):
+def write_cifar10_file(directory=DATASET_FOLDER):
     (x_train, _), (x_test, _) = cifar10.load_data()
-    n_train_samples, n_test_samples = _get_n_samples(n_samples)
-
+    
     train_dir = os.path.join(directory, "train", "negative")
-    for i, img in enumerate(x_train[:n_train_samples]):
+    for i, img in enumerate(x_train):
         to_file(img, train_dir, fname="{}.png".format(i+1))
     
     valid_dir = os.path.join(directory, "val", "negative")
-    for i, img in enumerate(x_test[:n_test_samples]):
+    for i, img in enumerate(x_test):
         to_file(img, valid_dir, fname="{}.png".format(i+1))
 
 
-def write_positive(directory=DATASET_FOLDER, n_samples=50000):
+def write_positive(directory=DATASET_FOLDER):
     def get_images():
         from src.utils import download
         from scipy.io import loadmat
@@ -63,10 +58,8 @@ def write_positive(directory=DATASET_FOLDER, n_samples=50000):
         return images
     
     images = get_images()
-    n_train_samples, n_test_samples = _get_n_samples(n_samples)
-    
-    x_train = images[:n_train_samples]
-    x_test = images[50000:50000+n_test_samples]
+    x_train = images[:50000]
+    x_test = images[50000:60000]
     
     train_dir = os.path.join(directory, "train", "text")
     for i, img in enumerate(x_train):
@@ -77,15 +70,9 @@ def write_positive(directory=DATASET_FOLDER, n_samples=50000):
         to_file(img, valid_dir, fname="{}.png".format(i+1))
 
 
-N_SAMPLES = 50   # per class
-
 if __name__ == "__main__":
-    import shutil
-    if os.path.exists(DATASET_FOLDER):
-        print("Deleting files....")
-        shutil.rmtree(DATASET_FOLDER)
-    build_dataset_tree(DATASET_FOLDER)
-    write_cifar10_file(n_samples = N_SAMPLES)
-    write_positive(n_samples = N_SAMPLES)
+    build_dataset_tree()
+    write_cifar10_file()
+    write_positive()
 
 
