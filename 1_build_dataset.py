ec3bcc38d6ee0455607896abbe26a54f1efe583a
jeongjoonsup <penny4860@gmail.com>
2018-01-22 19:49:55 +0900

    nit

--- 1_build_dataset.py
+++ 1_build_dataset.py
@@ -1,7 +1,11 @@
 #-*- coding: utf-8 -*-
+import os
 
 DATASET_TEXT = "dataset//train//text"
-DATASET_NEGATIVE = "dataset//train//text"
+DATASET_NEGATIVE = "dataset//train//negative"
 
-
+if __name__ == "__main__":
+    text_imgs = os.listdir(DATASET_TEXT)            # 350
+    negative_imgs = os.listdir(DATASET_NEGATIVE)    # 1225
+    print(len(text_imgs), len(negative_imgs))
 
