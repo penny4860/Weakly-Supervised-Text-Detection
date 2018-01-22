e3f74b2212f70a76059644565b61abb5e5cadd34
jeongjoonsup <penny4860@gmail.com>
2018-01-22 20:43:17 +0900

    utils.get_list_images() extracted

--- 2_features.py
+++ 2_features.py
@@ -1,25 +1,11 @@
 #-*- coding: utf-8 -*-
-import os
 import numpy as np
-import cv2
 from src.feature import FeatureExtractor
+from src.utils import get_list_images
 
 DATASET_TEXT = "dataset//train//text"
 DATASET_NEGATIVE = "dataset//train//negative"
 
-def get_list_images(folder):
-    def _get_files(folder):
-        files = os.listdir(folder)
-        files = [os.path.join(folder, fname) for fname in files]
-        return files
-    
-    files = _get_files(folder)
-    images = []
-    for fname in files:
-        img = cv2.imread(fname)
-        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-        images.append(img)
-    return images
 
 if __name__ == "__main__":
 
