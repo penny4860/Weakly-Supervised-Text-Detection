4cfc62ef43dd3f27dafbad5e23492926faa187f9
jeongjoonsup <penny4860@gmail.com>
2018-01-22 21:56:44 +0900

    nit

--- 4_activate.py
+++ 4_activate.py
@@ -11,23 +11,23 @@ if __name__ == "__main__":
 
 
     # Get postive features
-    positive_images = get_list_images(DATASET_TEXT)
+    positive_images = get_list_images(DATASET_TEXT)[20:50]
  
     fe = FeatureExtractor()
-    conv_output = fe.get_image_feature(positive_images[:32])
-    print(conv_output.shape)
+    conv_maps = fe.get_image_feature(positive_images)
+    print(conv_maps.shape)
  
     # get AMP layer weights
     model = load_model("cls.pkl")
     from src.activate import activate_label
     import cv2
     import matplotlib.pyplot as plt
-    for i, conv_map in enumerate(conv_output):
+    for img, conv_map in zip(positive_images, conv_maps):
         map_ = activate_label(conv_map, 0, model.coef_.reshape(-1,1), image_size=(224,224))
         fig, ax = plt.subplots()
-        im = cv2.resize(positive_images[i], (224, 224))
-        ax.imshow(im, alpha=0.8)
-        ax.imshow(map_, cmap='jet', alpha=0.2)
+        im = cv2.resize(img, (224, 224))
+        ax.imshow(im, alpha=0.6)
+        ax.imshow(map_, cmap='jet', alpha=0.4)
         plt.show()
 
 
