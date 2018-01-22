8387503fd43c18e9c13ea4bfa1678720804122fd
jeongjoonsup <penny4860@gmail.com>
2018-01-22 20:44:20 +0900

    nit

--- 2_features.py
+++ 2_features.py
@@ -11,12 +11,13 @@ if __name__ == "__main__":
 
     fe = FeatureExtractor()
     
-    # Get list of images
+    # Get postive features
     positive_images = get_list_images(DATASET_TEXT)
     positive_features = fe.run(positive_images)
     np.save("positive_features", positive_features)
     print(positive_features.shape)
 
+    # Get negative features
     negative_images = get_list_images(DATASET_NEGATIVE)
     negative_features = fe.run(negative_images)
     np.save("negative_features", negative_features)
