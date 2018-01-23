e641d27bc3b656cdf6112d4897321e6bb463934a
jeongjoonsup <penny4860@gmail.com>
2018-01-23 19:52:40 +0900

    ClsActWorker added

--- 4_activate.py
+++ 4_activate.py
@@ -1,5 +1,6 @@
 #-*- coding: utf-8 -*-
 import cv2
+import numpy as np
 import matplotlib.pyplot as plt
 
 from src.feature import FeatureExtractor
@@ -8,26 +9,41 @@ from src.activate import activate_label
 
 DATASET_TEXT = "dataset//train//text"
 
+
+class ClsActWorker(object):
+    
+    def __init__(self):
+        self._fe = FeatureExtractor()
+    
+    def run(self, images):
+        feature_images = self._fe.to_feature_image(images)
+
+        # get AMP layer weights
+        model = load_model("cls.pkl")
+        activation_maps = []
+        for feature_image in feature_images:
+            map_ = activate_label(feature_image,
+                                  0,
+                                  model.coef_.reshape(-1,1),
+                                  image_size=(224,224))
+            activation_maps.append(map_)
+        return np.array(activation_maps)
+        
 if __name__ == "__main__":
 
 
     # Get postive features
-    positive_images = get_list_images(DATASET_TEXT)[30:32]
+    images = get_list_images(DATASET_TEXT)[30:32]
+    
+    worker = ClsActWorker()
+    maps = worker.run(images)
  
-    fe = FeatureExtractor()
-    conv_maps = fe.to_feature_image(positive_images)
-    print(conv_maps.shape)
- 
-    # get AMP layer weights
-    model = load_model("cls.pkl")
-    for i, (img, conv_map) in enumerate(zip(positive_images, conv_maps)):
-        map_ = activate_label(conv_map, 0, model.coef_.reshape(-1,1), image_size=(224,224))
-        map_ = cv2.resize(map_, (img.shape[1], img.shape[0]))
-        
+    for img, conv_map in zip(images, maps):
+        conv_map = cv2.resize(conv_map, (img.shape[1], img.shape[0]))
         fig, ax = plt.subplots(nrows=2, ncols=2)
         plt.subplot(1, 2, 1)
         plt.imshow(img, alpha=0.6)
-        plt.imshow(map_, cmap='jet', alpha=0.4)
+        plt.imshow(conv_map, cmap='jet', alpha=0.4)
         plt.subplot(1, 2, 2)
         plt.imshow(img)
         plt.show()
