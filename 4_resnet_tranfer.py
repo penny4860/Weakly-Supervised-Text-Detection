aa54f115cd67ca678f32be3f45c76b59cf2b3e8a
jeongjoonsup <penny4860@gmail.com>
2018-01-25 19:43:31 +0900

    [to be reverted] negative images added

--- 4_resnet_tranfer.py
+++ 4_resnet_tranfer.py
@@ -1,23 +1,23 @@
 
-from keras.models import Model
-from keras.applications.resnet50 import ResNet50, preprocess_input
-from keras.layers import Dropout, Flatten, Dense, Conv2D, AveragePooling2D
-
+from keras.optimizers import Adam
 from src.resnet import resnet50_cam
+from src.utils import build_generator, create_callbacks
 
 if __name__ == "__main__":
     model = resnet50_cam()
+    model.summary()
     optimizer = Adam(lr=1e-3, beta_1=0.9, beta_2=0.999, epsilon=1e-08, decay=0.0)
-    model.compile(loss = 'categorical_crossentropy', optimizer = optimizer, metrics=['accuracy'])
-    
-    train_generator = build_generator("dataset//train")
-    # valid_generator = build_generator("dataset//valid")
+    model.compile(loss = 'categorical_crossentropy',
+                  optimizer = optimizer,
+                  metrics = ['accuracy'])
 
-    mobilenet.fit_generator(train_generator,
-                            steps_per_epoch = len(train_generator),
+    train_generator = build_generator("dataset//train")
+
+    model.fit_generator(train_generator,
+                        steps_per_epoch = len(train_generator),
 #                             validation_data = valid_generator,
 #                             validation_steps = len(valid_generator),
-                            callbacks        = create_callbacks(),
-                            epochs=20)
+                        callbacks = create_callbacks("resnet.h5"),
+                        epochs=20)
 
 
