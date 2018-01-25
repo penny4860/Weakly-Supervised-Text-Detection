c00c25691afe19bc40cf42c8ae6610643a89c3f1
jeongjoonsup <penny4860@gmail.com>
2018-01-25 20:09:43 +0900

    transfer false

--- 4_resnet_tranfer.py
+++ 4_resnet_tranfer.py
@@ -5,7 +5,7 @@ from src.resnet import resnet50_cam
 from src.utils import build_generator, create_callbacks
 
 if __name__ == "__main__":
-    model = resnet50_cam()
+    model = resnet50_cam(froze_pretrained_layer=False)
     model.summary()
     optimizer = Adam(lr=1e-3, beta_1=0.9, beta_2=0.999, epsilon=1e-08, decay=0.0)
     model.compile(loss = 'categorical_crossentropy',
