d92131f6b04e92a2ae3349775cb8c43a238bb116
jeongjoonsup <penny4860@gmail.com>
2018-01-25 19:47:00 +0900

    Revert "sample neagative images"
    This reverts commit fadbc20bdd0811ca50fc2c31fc6f4eb1f840db37.

--- 1_extract_features.py
+++ 1_extract_features.py
@@ -2,23 +2,30 @@
 import numpy as np
 from src.feature import FeatureExtractor
 from src.utils import files_to_images, list_files
-import shutil
-import os
 
 DATASET_TEXT = "dataset//train//text"
 DATASET_SVT = "..//dataset//svt1"
 DATASET_KAIST = "..//dataset//kaist"
-DATASET_NEGATIVE = "..//dataset//sun_dataset"
+DATASET_NEGATIVE = "..//dataset//images"
 
 # It takes about 15 minutes on the CPU.
 if __name__ == "__main__":
 
+    fe = FeatureExtractor()
+
+
     # Get negative features
-    negative_images = list_files(DATASET_NEGATIVE, "*.jpg", 1800)
-    dname = "dataset//train//negative"
+    files = list_files(DATASET_SVT, "*.jpg", n_files_to_sample=350, random_order=False)
+    positive_images = files_to_images(files)
+    positive_features = fe.to_feature_vector(positive_images)
+    np.save("svt_features", positive_features)
+    print(positive_features.shape) # (350, 2048)
     
-    for fname in negative_images:
-        new_name = os.path.join(dname, os.path.split(fname)[1])
-        shutil.copyfile(fname, new_name)
-        print(new_name)
+#     # Get negative features
+#     negative_images = get_list_images(DATASET_NEGATIVE, 1800)
+#     negative_features = fe.to_feature_vector(negative_images)
+#     np.save("negative_features", negative_features)
+#     print(negative_features.shape) # (1800, 2048)
+
+    
 
