5aac4a3e146d78c32e1ab715f161152a14765233
jeongjoonsup <penny4860@gmail.com>
2018-01-25 17:00:26 +0900

    batchnorom added

--- src/resnet.py
+++ src/resnet.py
@@ -2,8 +2,8 @@
 
 from keras.models import Model
 from keras.applications.resnet50 import ResNet50, preprocess_input
-from keras.layers import Dropout, Flatten, Dense, Conv2D, AveragePooling2D
-
+from keras.layers import Dropout, Flatten, Dense, Conv2D, AveragePooling2D, BatchNormalization, Activation
+from keras import backend as K
 
 def resnet50_cam(spatial_size=14, n_classes=2, froze_pretrained_layer=True):
     resnet = ResNet50(input_shape=(224, 224, 3), weights='imagenet')
@@ -17,17 +17,28 @@ def resnet50_cam(spatial_size=14, n_classes=2, froze_pretrained_layer=True):
 
     x = model.output
     
+    if K.image_data_format() == 'channels_last':
+        bn_axis = 3
+    else:
+        bn_axis = 1
+
     # Add another conv layer with ReLU + GAP
-    x = Conv2D(1024, (3,3), activation='relu', padding='same', name='cam_conv')(x)
+    x = Conv2D(1024, (3,3), padding='same', name='cam_conv')(x)
+    x = BatchNormalization(axis=bn_axis)(x)
+    x = Activation('relu')(x)
     x = AveragePooling2D(pool_size=(spatial_size,spatial_size),
                          name='cam_average_pooling')(x)
     x = Flatten()(x)
     x = Dense(n_classes, activation='softmax', name='cam_cls')(x)
     model = Model(model.input, x)
+    model.summary()
 
     if froze_pretrained_layer:
-        for layer in model.layers[:-4]:
+        for layer in model.layers[:-6]:
             layer.trainable = False
+            
+            print(layer.name)
+    
     return model
 
 
