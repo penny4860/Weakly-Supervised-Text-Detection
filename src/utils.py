7d1e1f1b193d3e658e02607d6a4f12725c8a4d2b
jeongjoonsup <penny4860@gmail.com>
2018-01-25 15:37:23 +0900

    utils.create_callbacks(), utils.build_generator() added

--- src/utils.py
+++ src/utils.py
@@ -84,3 +84,30 @@ def list_files(directory, pattern="*.*", n_files_to_sample=None, recursive_optio
     return files
 
 
+def create_callbacks(weight_file="mobilenet_cls2.h5"):
+    from keras.callbacks import ModelCheckpoint
+    checkpoint = ModelCheckpoint("weights.{epoch:02d}-{loss:.2f}.h5", 
+                                 monitor='loss', 
+                                 verbose=1, 
+                                 save_best_only=False,
+                                 mode='min', 
+                                 period=1)
+    return [checkpoint]
+
+
+def build_generator(directory, preprocess_input, augment=False):
+    if augment == True:
+        data_generator = ImageDataGenerator(preprocessing_function=preprocess_input,
+                                           rotation_range=20.0,
+                                           width_shift_range=0.2,
+                                           height_shift_range=0.2,
+                                           shear_range=0.4,
+                                           zoom_range=0.2)
+    else:
+        data_generator = ImageDataGenerator(preprocessing_function=preprocess_input)
+
+    generator = data_generator.flow_from_directory(directory=directory,
+                                                        target_size=(224, 224),
+                                                        batch_size=8)
+    return generator
+
