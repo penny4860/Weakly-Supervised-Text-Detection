8ff502b68a54a714b084f7cf4b9bbaf34e049796
jeongjoonsup <penny4860@gmail.com>
2018-01-27 20:50:54 +0900

    nit

--- 1_train.py
+++ 1_train.py
@@ -8,6 +8,18 @@ from src.keras_utils import build_generator, create_callbacks
 from keras.models import Model
 from keras.layers import AveragePooling2D, Flatten, Dense
 
+def get_model():
+    builder = CamModelBuilder()
+    model = builder._resnet
+    model = Model(inputs=model.input, outputs=model.get_layer("activation_40").output)
+
+    x = model.output
+    x = AveragePooling2D(pool_size=(14, 14),
+                         name='cam_average_pooling')(x)
+    x = Flatten()(x)
+    x = Dense(2, activation='softmax', name='cam_cls')(x)
+    model = Model(model.input, x)
+    return model
 
 if __name__ == "__main__":
     model = get_model()
