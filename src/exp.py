c6bc8573f6c34c24c57d60435e40a28367c60f2c
jeongjoonsup <penny4860@gmail.com>
2018-01-27 21:03:11 +0900

    get_model_conv_14x14 added

--- src/exp.py
+++ src/exp.py
@@ -1,7 +1,7 @@
 
 
 from keras.models import Model
-from keras.layers import AveragePooling2D, Flatten, Dense
+from keras.layers import AveragePooling2D, Flatten, Dense, Conv2D
 from keras.applications.resnet50 import ResNet50, preprocess_input
 
 def get_model_14x14():
@@ -15,3 +15,17 @@ def get_model_14x14():
     x = Dense(2, activation='softmax', name='cam_cls')(x)
     model = Model(model.input, x)
     return model
+
+def get_model_conv_14x14():
+    model = ResNet50()
+    model = Model(inputs=model.input, outputs=model.get_layer("activation_40").output)
+
+    x = model.output
+    x = Conv2D(1024, (3,3), padding='same', activation="relu")(x)
+    x = AveragePooling2D(pool_size=(14, 14),
+                         name='cam_average_pooling')(x)
+    x = Flatten()(x)
+    x = Dense(2, activation='softmax', name='cam_cls')(x)
+    model = Model(model.input, x)
+    return model
+
