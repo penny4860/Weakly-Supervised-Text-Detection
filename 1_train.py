e355fa12f99268afb5a221bf840bd4c638595e9f
jeongjoonsup <penny4860@gmail.com>
2018-01-27 20:27:37 +0900

    14x14 activation 이후에 바로 final

--- 1_train.py
+++ 1_train.py
@@ -5,10 +5,24 @@ from keras.optimizers import Adam
 from keras.applications.resnet50 import preprocess_input
 from src.keras_utils import build_generator, create_callbacks
 
-if __name__ == "__main__":
+from keras.models import Model
+from keras.layers import AveragePooling2D, Flatten, Dense
+
+def get_model():
     builder = CamModelBuilder()
-    model = builder.get_cls_model()
-    model.summary()
+    model = builder._resnet
+    model = Model(inputs=model.input, outputs=model.get_layer("activation_40").output)
+
+    x = model.output
+    x = AveragePooling2D(pool_size=(14, 14),
+                         name='cam_average_pooling')(x)
+    x = Flatten()(x)
+    x = Dense(2, activation='softmax', name='cam_cls')(x)
+    model = Model(model.input, x)
+    return model
+
+if __name__ == "__main__":
+    model = get_model()
 
     fixed_layers = []
     for layer in model.layers[:-1]:
@@ -20,7 +34,7 @@ if __name__ == "__main__":
     model.compile(loss = 'categorical_crossentropy',
                   optimizer = optimizer,
                   metrics = ['accuracy'])
-     
+      
     train_generator = build_generator("dataset//train", preprocess_input, augment=True)
     model.fit_generator(train_generator,
                         steps_per_epoch = len(train_generator),
