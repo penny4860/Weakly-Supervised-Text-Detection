66b1678fbbf31f0ddfcd6dfb1f914f0ecfec5635
jeongjoonsup <penny4860@gmail.com>
2018-01-28 12:48:13 +0900

    Revert "28x28x512 까지 fix"
    This reverts commit 8ad09286f4636f5657d09bd7ec29d41375954a73.

--- 1_train.py
+++ 1_train.py
@@ -10,11 +10,11 @@ if __name__ == "__main__":
     model = get_model_conv_14x14()
     model.summary()
 
-    fixed_layers = []
-    for layer in model.layers[:79]:
-        layer.trainable = False
-        fixed_layers.append(layer.name)
-    print(fixed_layers)
+#     fixed_layers = []
+#     for layer in model.layers[:-6]:
+#         layer.trainable = False
+#         fixed_layers.append(layer.name)
+#     print(fixed_layers)
  
     optimizer = Adam(lr=1e-3, beta_1=0.9, beta_2=0.999, epsilon=1e-08, decay=0.0)
     model.compile(loss = 'categorical_crossentropy',
